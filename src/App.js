606d9536d3b4f0d4ecda8f976ebfc739904f667e
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-11-28 11:44:54 +0700

    use routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import routes from './config/routes'
+import styled from 'styled-components'
+import { Reset } from 'styled-reset'
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <GlobalStyle />
+      <Reset />
+      <Router>
+        <Suspense fallback='...loading'>
+          <Switch>
+            {Object.keys(routes).map(routeKey => (
+              <Route Key={routeKey} {...routes[routeKey]} />
+            ))}
+          </Switch>
+        </Suspense>
+      </Router>
+    </>
+  )
 }
 
-export default App;
+export default App
