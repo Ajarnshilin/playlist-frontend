f7089b4f4c146d1bf78b696ba59f089f3ca008ce
naratipcover <naratipcover@gmail.com>
2020-11-28 15:03:20 +0700

    add login page

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+// import styled from "styled-components";
+import Sign from "../components/SignStyle"
+
+export default function Login() {
+  return (
+    <>
+      <Sign/>
+    </>
+  );
+}
