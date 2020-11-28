c3c960d1cee773df3a550e2c976b556124aebd39
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-11-28 11:55:38 +0700

    delete styled & global

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import Popup from './components/Popup'
 import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import routes from './config/routes'
-import styled from 'styled-components'
 import { Reset } from 'styled-reset'
 
 function App () {
   return (
     <>
-      <GlobalStyle />
       <Reset />
       <Router>
         <Suspense fallback='...loading'>
           <Switch>
-            <Popup/>
+            <Popup />
             {Object.keys(routes).map(routeKey => (
               <Route Key={routeKey} {...routes[routeKey]} />
             ))}
