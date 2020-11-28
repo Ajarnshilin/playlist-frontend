acff463286f72347506cf31d2d575cb816775449
branzbbest <rattaphoom_eikumnerd@hotmail.com>
2020-11-28 16:26:54 +0700

    add remove button

--- src/components/Content.js
+++ src/components/Content.js
@@ -144,13 +144,13 @@ function Content() {
                 <SongTitleContainer>{item.title}</SongTitleContainer>
                 <SongArtistContainer>{item.artist}</SongArtistContainer>
                 <SongDurationContainer>{item.duration}</SongDurationContainer>
+                <Remove type="submit">Remove</Remove>
+                {/* <Add type="submit">Remove</Add> */}
               </SongContainer>
             ))}
         </Text>
       </Wrapper>
 
-      <Remove type="submit">Remove</Remove>
-      <Add type="submit">Remove</Add>
 
     </Container>
   );
