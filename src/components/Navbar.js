617aace98c0896a498b5995e7979cff76de54dbf
DogeCnx <Saratoga1412@gmail.com>
2020-11-28 16:23:26 +0700

    ADD api and playlist

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { getPlaylist } from "./../services/APIsong";
 
 const Container = styled.div`
   width: 15%;
@@ -10,6 +11,7 @@ const Container = styled.div`
   padding: 0;
 `;
 
+
 const Wrapper = styled.div``;
 
 const ActionContainer = styled.div``;
@@ -17,13 +19,21 @@ const ActionContainer = styled.div``;
 const Action = styled.div``;
 
 function NavBar() {
+  const [play ,setPlay ] = useState([])
+
+  useEffect(() => {
+    getPlaylist(1).then(response => setPlay(response))
+  } ,[])
+  console.log(play)
+
   return (
     <Container>
       <Wrapper>
         <ActionContainer>
           <Action>
-            <Link to="/Newplaylist">+ New Playlist</Link>
+            <Link to="/Newplaylist">+ New Playlist</Link>          
           </Action>
+             {play.map( (item) => (<p>{item.name}</p>))}  
         </ActionContainer>
       </Wrapper>
     </Container>
