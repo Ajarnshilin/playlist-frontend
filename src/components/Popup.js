b25006d3658bedd5e5a76ad313317523c369addd
Ajarnnotes <ajarnnotes@gmail.com>
2020-11-28 11:37:11 +0700

    add components

---
+++ src/components/Popup.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "styled-components";
+
+const Backgroud = styled.div`
+  background-color: black;
+  opacity: 0.6;
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+`;
+const Container = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 100;
+`;
+const Box = styled.div`
+  padding: 1rem;
+  width: 500px;
+  height: 350px;
+  background-color: yellow;
+
+  h1 {
+    text-align: left;
+    font-weight: bold;
+  }
+`;
+
+function PopUp() {
+  return (
+    <>
+      <Backgroud />
+      <Container>
+        <Box>
+          <h1>Create Playlist</h1>
+        </Box>
+      </Container>
+    </>
+  );
+}
+export default PopUp;
